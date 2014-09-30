d21a92126b6da424ab77c2f0c8af67ca69cbae64
Hugh Nguyen <hugh.hn@gmail.com>
2014-09-29 19:22:14 -0700

    first commit

---
+++ src/com/codepath/hughhn/simplechat/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.hughhn.simplechat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ChatActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
---
+++ src/com/codepath/hughhn/simplechat/ChatApplication.java
@@ -0,0 +1,16 @@
+package com.codepath.hughhn.simplechat;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+
+public class ChatApplication extends Application {
+	public static final String YOUR_APPLICATION_ID = "xclOs7U9O3xZ3SfCViGJDDZ6Bg09t8tAprmPp58i";
+	public static final String YOUR_CLIENT_KEY = "3ftLL5u2rS9x59UelvoNg1kDN169QT50WXL2X09e";
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
+	}
+}
