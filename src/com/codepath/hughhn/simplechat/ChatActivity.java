0a9448403968e497ddd06550b8ba4c9f5de04738
Sushant Narsale <sushant.narsale2603@gmail.com>
2014-09-29 19:46:37 -0700

    Anonymous Login

--- src/com/codepath/hughhn/simplechat/ChatActivity.java
+++ src/com/codepath/hughhn/simplechat/ChatActivity.java
@@ -1,15 +1,40 @@
 package com.codepath.hughhn.simplechat;
 
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
 public class ChatActivity extends Activity {
+
+	private static String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+          userId = ParseUser.getCurrentUser().getObjectId();
+        } else {
+        	login();
+        }
+    }
+
+    private void login() {
+    	ParseAnonymousUtils.logIn(new LogInCallback() {
+    		  @Override
+    		  public void done(ParseUser user, ParseException e) {
+    		    if (e != null) {
+    		      Log.d("MyApp", "Anonymous login failed.");
+    		    } else {
+    		      ChatActivity.this.userId = user.getObjectId();
+    		    }
+    		  }
+    		});
     }
 }
